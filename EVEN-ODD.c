59bf82029f6bf725a019d1db94462dc6daf22bad
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2025-09-18 20:52:15 +0530

    Create EVEN-ODD.c

---
+++ EVEN-ODD.c
@@ -0,0 +1,17 @@
+void main(){
+int num;
+printf("enter number:\n");
+scanf("%d",&num);
+
+if(num%2==0)
+{
+    printf("%d is EVEN\n",num);
+}
+else
+{
+    printf("%d is ODD\n",num);
+}
+getch();
+}
+
+
